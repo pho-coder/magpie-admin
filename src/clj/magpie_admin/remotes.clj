7252acf47a4b95586ac82b5f0d0aebbcf5cd0d2e
Phoenix Zhang <pho.coderzhang@gmail.com>
2014-04-02 18:04:17 +0800

    f

--- src/clj/magpie_admin/remotes.clj
+++ src/clj/magpie_admin/remotes.clj
@@ -1,11 +1,13 @@
 (ns magpie-admin.remotes
   (:require [compojure.handler :refer [site]]
             [shoreleave.middleware.rpc :refer [defremote wrap-rpc]]
-
+            [clojure.tools.logging :refer [info]]
+            
             [magpie-admin.handler :refer [handler]]
             [magpie-admin.state-info :refer [start-tracking *supervisors-info* *tasks-info* *offset-info*]]))
 
 (defremote rpc-get-info []
+  (info "rpc get info")
   {:supervisors @*supervisors-info*
    :tasks @*tasks-info*
    :offset @*offset-info*})
