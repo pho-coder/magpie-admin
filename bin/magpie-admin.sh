a8e616ea24fd186c33e2516d008fcdfb88a7e0e6
Phoenix Zhang <pho.coderzhang@gmail.com>
2014-04-03 11:06:51 +0800

    f

--- bin/magpie-admin.sh
+++ bin/magpie-admin.sh
@@ -2,4 +2,4 @@
 MAGPIE_HOME=/export/jrdw/magpie-admin
 LOGS_PATH=${MAGPIE_HOME}/logs
 VERSION=0.1.0
-nohup java -Xmx256M -Xms128M -jar -DLOGS_PATH=${LOGS_PATH} magpie-admin-${VERSION}-SNAPSHOT-standalone.jar 2>&1 < /dev/null &
+nohup java -Xmx256M -Xms128M -jar -DLOGS_PATH=${LOGS_PATH} ${MAGPIE_HOME}/target/magpie-admin-${VERSION}-SNAPSHOT-standalone.jar 2>&1 < /dev/null &
